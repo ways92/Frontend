462bdf8509a2f7ad278f946a84323052adc1c987
Ways Alqorni <57651690+ways92@users.noreply.github.com>
2021-09-26 11:16:32 +0700

    Add files via upload
    updated september

---
+++ Daftar Tugas (Vue Js)/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Tambah v-on:tambah-tugas="Tambah" />
+    <Hasil v-on:delete-hasil="deleteHasil" v-bind:hasil="hasil" />
+  </div>
+</template>
+
+<script>
+import Tambah from "./components/Tambah.vue";
+import Hasil from "./components/Hasil.vue";
+
+export default {
+  name: "App",
+  components: {
+    Tambah,
+    Hasil,
+  },
+  data() {
+    return {
+      hasil: [
+        {
+          id: "1",
+          matakuliah: "Matematika",
+          haritenggat: "Selasa",
+        },
+        {
+          id: "2",
+          matakuliah: "Biologi",
+          haritenggat: "Senin",
+        },
+        {
+          id: "3",
+          matakuliah: "Kimia",
+          haritenggat: "Kamis",
+        },
+        {
+          id: "4",
+          matakuliah: "Fisika",
+          haritenggat: "Rabu",
+        },
+      ],
+    };
+  },
+  methods: {
+    Tambah(newTugas) {
+      this.hasil = [...this.hasil, newTugas];
+    },
+    deleteHasil(id) {
+      this.hasil = this.hasil.filter((hasi) => hasi.id !== id);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ Daftar Tugas (Vue Js)/src/components/Hasil.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="row text-center mt-5 ms-5 me-5">
+      <div class="col">
+        <h2>Mata Kuliah</h2>
+      </div>
+      <div class="col">
+        <h2>Hari Tenggat</h2>
+      </div>
+      <div class="col">
+        <h2>Aksi</h2>
+      </div>
+    </div>
+    <div v-for="hasi in hasil" v-bind:key="hasi.id">
+      <IsiTugas v-bind:id="hasi.id" v-bind:matakuliah="hasi.matakuliah" v-bind:haritenggat="haritenggat" v-bind:hasi="hasi" v-bind:hasil="hasil" v-on:delete-hasil="$emit('delete-hasil', hasi.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import IsiTugas from "./IsiTugas.vue";
+export default {
+  name: "Hasil",
+  components: {
+    IsiTugas,
+  },
+  props: ["hasil"],
+};
+</script>
+
+<style scoped></style>
